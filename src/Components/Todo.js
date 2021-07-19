19f94c800cad06cc5cfa6324bc8373b22b2fb2b7
amirbasiri2000 <78272810+amirbasiri2000@users.noreply.github.com>
2021-07-19 11:01:43 +0430

    project base

---
+++ src/Components/Todo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.css';
+
+
+const Todo = () =>{
+
+     
+
+    return(
+        <div className="todo">
+            <li className="todo-item">amir basiri</li>
+            <button className="complete-btn">
+                <i className="fas fa-check"></i>
+            </button>
+            <button className="trash-btn">
+                <i className="fas fa-trash"></i>
+            </button>
+        </div>
+    );
+}
+
+
+export default Todo;
